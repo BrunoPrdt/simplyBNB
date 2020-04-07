ffb6e35251130ce4cd6c919077e859d888725da3
Bruno <bruno.predot@gmail.com>
2020-04-07 20:34:28 +0200

    commit to first complete save on github

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller {
+
+
+    /**
+     * @Route("/", name="homepage")
+     * @param AdRepository $adRepo
+     * @param UserRepository $userRepo
+     * @return Response
+     */
+    public function home(AdRepository $adRepo, UserRepository $userRepo):Response
+    {
+        return $this->render(
+            'home.html.twig', 
+            [
+                'ads' => $adRepo->findBestAds(3),
+                'users' => $userRepo->findBestUsers(2)
+            ]
+        );
+    }
+}
+
+?>
